31ebf3f0563722344212ea1b196661f3e497d123
Danyi Aprizal <danylapriza3@gmail.com>
2025-05-12 20:15:20 +0700

    Halaman Daftar dan Login Akun

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,20 @@ import Home from './pages/Home';
 import Booking from './pages/Booking';
 import Payment from './pages/Payment';
 import Confirmation from './pages/Confirmation';
+import Daftar from './pages/Daftar';
+import Masuk from './pages/masuk';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/home" />} /> {/*Initial loading page*/}
+      <Route path="/" element={<Navigate to="/home" />} /> 
       <Route path="/home" element={<Home />} />
       <Route path="/booking" element={<Booking />} />
       <Route path="/payment" element={<Payment />} />
       <Route path="/confirmation" element={<Confirmation />} />
+      <Route path="/Daftar" element={<Daftar />} />
+      <Route path="/masuk" element={<Masuk />} />
+
     </Routes>
   );
 }
